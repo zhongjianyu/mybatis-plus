d52525dfb4ba419a171394f4bf5f51ab7d9bee4f
zhongjianyu <949434438@qq.com>
2018-01-09 09:36:37 +0800

    add

--- aljoin-mybatis-plus-support/src/main/java/com/baomidou/mybatisplus/toolkit/SqlReservedWords.java
+++ aljoin-mybatis-plus-support/src/main/java/com/baomidou/mybatisplus/toolkit/SqlReservedWords.java
@@ -147,7 +147,13 @@ public class SqlReservedWords {
         Collections.addAll(RESERVED_WORDS, words);
         
         // 自定义oracle关键字
-        String[] oracleWords = {"COMMENT","VERSION"};
+        String[] oracleWords = {"ACCESS","ADD","ALL","ALTER","AND","ANY","AS","ASC","AUDIT","BETWEEN","BY","CHAR","CHECK","CLUSTER","COLUMN","COMMENT","COMPRESS",
+                "CONNECT","CREATE","CURRENT","DATE","DECIMAL","DEFAULT","DELETE","DESC","DISTINCT","DROP","ELSE","EXCLUSIVE","EXISTS","FILE","FLOAT",
+                "FOR","FROM","GRANT","GROUP","HAVING","IDENTIFIED","IMMEDIATE","IN","INCREMENT","INDEX","INITIAL","INSERT","INTEGER","INTERSECT","INTO",
+                "IS","LEVEL","LIKE","LOCK","LONG","MAXEXTENTS","MINUS","MLSLABEL","MODE","MODIFY","NOAUDIT","NOCOMPRESS","NOT","NOWAIT","NULL","NUMBER",
+                "OF","OFFLINE","ON","ONLINE","OPTION","OR","ORDER","P","CTFREE","PRIOR","PRIVILEGES","PUBLIC","RAW","RENAME","RESOURCE","REVOKE","ROW",
+                "ROWID","ROWNUM","ROWS","SELECT","SESSION","SET","SHARE","SIZE","SMALLINT","START","SUCCESSFUL","SYNONYM","SYSDATE","TABLE","THEN","TO",
+                "TRIGGER","UID","UNION","UNIQUE","UPDATE","USER","VALIDATE","VALUES","VARCHAR","VARCHAR2","VIEW","WHENEVER","WHERE","WITH","VERSION"};
         ORACLE_RESERVED_WORDS = new HashSet<>(oracleWords.length);
         Collections.addAll(ORACLE_RESERVED_WORDS, oracleWords);
     }
